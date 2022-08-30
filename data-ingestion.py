4307b30e226bc74b87eb944c3d65fd6d68464df4
ramon <110137343+rayou2@users.noreply.github.com>
2022-08-30 18:02:33 -0400

    finsihed

--- data-ingestion.py
+++ data-ingestion.py
@@ -1,4 +1,3 @@
-from gettext import install
 import pandas as pd ## import pandas for general file types 
 import json ## imoprt json for json files
 import bs4 ## import bs4 for html files
@@ -16,8 +15,8 @@ import PyPDF2 ## import PyPDF2 for pdf files
 
 ## Section 1 
 ## open excel file
-tab1 = pd.read_excel('python/data1.xls',sheet_name='Sheet1') ## read excel file
-tab2 = pd.read_excel('python/data1.xls',sheet_name='Sheet2') ## read excel file
+tab1 = pd.read_excel("data/data1.xls",sheet_name='Sheet1') ## read excel file
+tab2 = pd.read_excel('data/data1.xls',sheet_name='Sheet2') ## read excel file
 print (tab1,'\n',tab2) ## prints the data
 
 
@@ -29,7 +28,17 @@ data = data.json() ## turns data into json
 print (data) ## prints the data
 
 ## Section 3
-pip install --upgrade google-cloud-bigquery
-from google.cloud import bigquery
+# pip install --upgrade google-cloud-bigquery
 
-client = bigquery.Client.from_service_account_json("data/ramon-507-2509a55a26d6.json") ## connects to GCP
+client = bigquery.Client.from_service_account_json('data/ramon-507-2509a55a26d6.json') ## connects to GCP
+query_job = client.query("SELECT * FROM `bigquery-public-data.breathe.arxiv` LIMIT 100") 
+## we used client query which wraps a sql query that selects all the column on breathe data and limited to the first 100 rows.
+results = query_job.result() ## results is a function that displays the results of the above query
+bigquery1 = pd.DataFrame(results.to_dataframe()) ## put results into dataframe
+print(bigquery1)
+
+query_job2 = client.query("SELECT * FROM `bigquery-public-data.austin_crime.crime` LIMIT 100")
+## we used client query which wraps a sql query that selects all the column on crime data and limited to the first 100 rows.
+results2 = query_job2.result()
+bigquery2 = pd.DataFrame(results2.to_dataframe())
+print(bigquery2)
